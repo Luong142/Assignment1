40e70d1e5f4c1ad513224cb47a85366eb1ebfb0f
Liang Luong <66522372+Luong142@users.noreply.github.com>
2024-04-29 21:30:48 +0800

    Reapply "update script prepare"
    This reverts commit f89f157e70148cf49560de2c16cfc45893773fd2.

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    public void OnPlayVideo()
+    {
+        Debug.Log("Hello World!");
+    }
+}
